83e3d94656a649b6a8748927c8b94969fcaf933b
Nidhi <149957057+Nidhu2212@users.noreply.github.com>
2023-11-27 03:12:42 -0500

    Add files via upload

---
+++ SQL Assessment 2.sql
@@ -0,0 +1,23 @@
+SELECT * FROM sstudent.student;
+-- 1 To display all the records form STUDENT table. SELECT * FROM student ; 
+SELECT *
+FROM STUDENT;
+
+-- 2. To display any name and date of birth from the table STUDENT. SELECT StdName, DOB FROM student ; 
+SELECT StdName, DOB
+FROM STUDENT;
+
+-- 3. To display all students record where percentage is greater of equal to 80 FROM student table.SELECT * FROM student WHERE percentage >= 80;
+SELECT *
+FROM student
+WHERE percentage >= 80;
+
+-- 4. To display student name, stream and percentage where percentage of student is more than 80 SELECT StdName, Stream, Percentage WHERE percentage > 80; 
+SELECT StdName, Stream, Percentage
+FROM student
+WHERE Percentage > 80;
+
+-- 5. To display all records of science students whose percentage is more than 75 form student table.SELECT * FORM student WHERE stream = ‘Science’ AND percentage > 75; 
+SELECT *
+FROM student
+WHERE Stream = 'Science' AND Percentage > 75;
